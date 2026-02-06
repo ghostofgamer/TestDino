5b95eb421d29b0905e080d55a644d1799cb05518
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2026-02-06 21:41:39 +0400

    Completed

--- Assets/Scripts/PuzzleHandler.cs
+++ Assets/Scripts/PuzzleHandler.cs
@@ -4,64 +4,88 @@ using UnityEngine;
 
 public class PuzzleHandler : MonoBehaviour
 {
-    [SerializeField]
-    private ParticleHandler _particleHandler;
-    [SerializeField]
-    private GameObject completePuzzle;
-    [SerializeField]
-    private GameObject puzzleBackground;
+    [SerializeField] private ParticleHandler _particleHandler;
+    [SerializeField] private GameObject completePuzzle;
+    [SerializeField] private GameObject puzzleBackground;
 
     private int piecesCount = 0;
 
-    [SerializeField]
-    private List<Vector2> positions = new List<Vector2>();
+    [SerializeField] private List<Vector2> positions = new List<Vector2>();
 
-    [SerializeField]
-    private float correctPositionAccuracy = 0.5f;
-    [SerializeField]
-    private float animationAccuracy = 0.01f;
-    [SerializeField]
-    private float smoothDragMultiplier = 10f;
-    [SerializeField]
-    private float minSmoothAnimationMultiplier = 2.5f;
-    [SerializeField]
-    private float maxSmoothAnimationMultiplier = 4f;
-    [SerializeField]
-    private float shadowAnimationDuration = 0.5f;
-    [SerializeField]
-    private float shakeTime = 0.05f;
-    [SerializeField]
-    private float shakeMultiplier = 5f;
+    [SerializeField] private float correctPositionAccuracy = 0.5f;
+    [SerializeField] private float animationAccuracy = 0.01f;
+    [SerializeField] private float smoothDragMultiplier = 10f;
+    [SerializeField] private float minSmoothAnimationMultiplier = 2.5f;
+    [SerializeField] private float maxSmoothAnimationMultiplier = 4f;
+    [SerializeField] private float shadowAnimationDuration = 0.5f;
+    [SerializeField] private float shakeTime = 0.05f;
+    [SerializeField] private float shakeMultiplier = 5f;
 
     public delegate void OnPlayerWin();
+
     public event OnPlayerWin onPlayerWin;
 
-    [SerializeField]
-    private bool useMouse;
+    [SerializeField] private bool useMouse;
 
-    public bool UseMouse { get => useMouse; }
-    public float CorrectPositionAccuracy { get => correctPositionAccuracy; }
-    public float AnimationAccuracy { get => animationAccuracy; }
-    public float SmoothDragMultiplier { get => smoothDragMultiplier; }
-    public float MinSmoothAnimationMultiplier { get => minSmoothAnimationMultiplier; }
-    public float MaxSmoothAnimationMultiplier { get => maxSmoothAnimationMultiplier; }
-    public float ShadowAnimationDuration { get => shadowAnimationDuration; }
-    public float ShakeTime { get => shakeTime; }
-    public float ShakeMultiplier { get => shakeMultiplier; }
+    public bool UseMouse
+    {
+        get => useMouse;
+    }
 
-    
+    public float CorrectPositionAccuracy
+    {
+        get => correctPositionAccuracy;
+    }
+
+    public float AnimationAccuracy
+    {
+        get => animationAccuracy;
+    }
+
+    public float SmoothDragMultiplier
+    {
+        get => smoothDragMultiplier;
+    }
+
+    public float MinSmoothAnimationMultiplier
+    {
+        get => minSmoothAnimationMultiplier;
+    }
+
+    public float MaxSmoothAnimationMultiplier
+    {
+        get => maxSmoothAnimationMultiplier;
+    }
+
+    public float ShadowAnimationDuration
+    {
+        get => shadowAnimationDuration;
+    }
+
+    public float ShakeTime
+    {
+        get => shakeTime;
+    }
+
+    public float ShakeMultiplier
+    {
+        get => shakeMultiplier;
+    }
+
+
     public GameObject realAnimal;
-    
+
     private void Start()
     {
         realAnimal = transform.GetChild(0).gameObject;
         realAnimal.SetActive(false);
-        
-        
+
+
         completePuzzle.SetActive(false);
-        
-#if  UNITY_WEBGL
-        useMouse = true;
+
+#if UNITY_WEBGL
+        if (!Application.isMobilePlatform)
+            useMouse = true;
 #endif
     }
 
@@ -75,7 +99,7 @@ public class PuzzleHandler : MonoBehaviour
     {
         piecesCount--;
 
-        if(piecesCount == 0)
+        if (piecesCount == 0)
         {
             completePuzzle.SetActive(true);
             puzzleBackground.SetActive(false);
